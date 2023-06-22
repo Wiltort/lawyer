13508be0578706668d0198317f6d0c8951fdc0ca
Виктор Королев <wiltort21@gmail.com>
2023-06-22 21:04:30 +0500

    Шаблоны начало

--- lawyer/lawyer/settings.py
+++ lawyer/lawyer/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -52,11 +52,11 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'lawyer.urls'
-
+TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,7 +117,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- lawyer/services/views.py
+++ lawyer/services/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Service, Post
 
 # Create your views here.
+def index(request):
+    srv_list = Service.objects.all()
+    last_news = Post.objects.order_by('-pub_date')[:5]
+    context = {'srv_list': srv_list, 'last_news': last_news}
+    return render(request, 'index.html', context)
